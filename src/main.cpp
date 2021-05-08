323179ed09b57c78e6dd730d84e9b8d764ecf3b7
Thomas Estabrook <thomas.r.estabrook@gmail.com>
2021-05-08 13:19:44 -0400

    +Add CLI user X,Y cooridinates to route planner

--- src/main.cpp
+++ src/main.cpp
@@ -51,16 +51,27 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+
+    float start_x = 0;
+    float start_y = 0;
+    float end_x = 0;
+    float end_y = 0;
+
+    std::cout << "Starting postion X Value: ";
+    std::cin >> start_x;
+    std::cout << "Starting postion Y Value: ";
+    std::cin >> start_y;
+    std::cout << "Ending postion X Value: ";
+    std::cin >> end_x;
+    std::cout << "Ending postion Y Value: "; 
+    std::cin >> end_y;
+   
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
